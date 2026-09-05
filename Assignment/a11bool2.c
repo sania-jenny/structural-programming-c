6e26d765ad481c5943b95cfdc69f0ccaa54e09f5
sania-jenny <saniajenny6@gmail.com>
2026-09-05 13:48:03 +0600

    add a11bool2.c

---
+++ Assignment/a11bool2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+
+
+    printf("%d\n" , 10==100);//print 0(False)
+    printf("%d\n" , 10==8);//print 0(False)
+    printf("%d\n" , 10>8);//print 0(False)
+    printf("%d\n" , 10<89);//print 0(False)
+
+
+return 0;
+}
