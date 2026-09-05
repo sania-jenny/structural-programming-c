a61c51c2b740d425ef4ea1b24028c02d56f3ff86
sania-jenny <saniajenny6@gmail.com>
2026-09-05 14:09:43 +0600

    add a11bool3.c

---
+++ Assignment/a11bool3.c
@@ -0,0 +1,22 @@
+
+#include<stdbool.h>
+int main(){
+    //A boolean variable is declared with the bool keyword and can take the values true or false:
+
+    bool france_fries_is_tasty=true;
+    bool pizza_is_tasty=false;
+
+    printf("%d\n" , france_fries_is_tasty);
+    printf("%d\n" ,pizza_is_tasty);
+    printf("%d\n" , france_fries_is_tasty==pizza_is_tasty);
+    printf("%d\n" , france_fries_is_tasty >pizza_is_tasty);
+    printf("%d\n" , france_fries_is_tasty >=pizza_is_tasty);
+      printf("%d\n" , france_fries_is_tasty<pizza_is_tasty);
+
+
+
+
+return 0;
+}
+
+
