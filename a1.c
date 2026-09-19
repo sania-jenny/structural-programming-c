5daf02fd423727e079f0115305d3a71e7552a231
sania-jenny <saniajenny6@gmail.com>
2026-09-19 14:43:10 +0600

    remove a1-a7.c

--- a1.c
+++
@@ -1,8 +0,0 @@
-//print block F and C
-#include<stdio.h>
-
-int main(){
-    printf("######\n#\n#\n#####\n#\n#\n#");
-    printf("\n\n\n   #####\n ##\t ##\n#\n#\n#\n#\n#\n#\n ##\t  ##\n   ######   ");
-        return 0;
-}
