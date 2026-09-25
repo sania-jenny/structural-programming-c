2eba8e6eb573083adf866ad2a86d8ecbfff27aa0
sania-jenny <saniajenny6@gmail.com>
2026-09-26 02:51:13 +0600

    upload copy2.c

---
+++ string/strcpy/copy2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char org []="alhamdulillah";
+    char cpy[20];
+    strcpy(cpy,org);
+    printf("the org is: %s \n",org);
+     printf("the cpy is: %s \n",cpy);
+
+     return 0;
+
+
+
+}
