40e3a4f216b8c34064e9ceca5b5972304d388b8f
sania-jenny <saniajenny6@gmail.com>
2026-09-26 02:35:04 +0600

    upload lenght1.c

---
+++ string/strlen/lenght1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char clg[]="sylhet engineering collage";
+    int lenght=strlen(clg);
+    printf("the lenght of string is :%d ",lenght);
+}
