c500cec2521ce3e74659395054d36fbbec2523c8
sania-jenny <saniajenny6@gmail.com>
2026-09-26 00:30:40 +0600

    upload to_get_er.c

---
+++ string/basic/to_get_er.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    char name[]={'s','a','n','i','a','\0'};
+    printf("my first name is : %s\n",name);
+
+    char nm[]={'A','k','t','e','r','\0'};
+    printf("my second name is : %s\n",nm);
+
+    char name2[]={'j','e','n','n','y','\0'};
+    printf("my third name is : %s",name2);
+
+
+}
